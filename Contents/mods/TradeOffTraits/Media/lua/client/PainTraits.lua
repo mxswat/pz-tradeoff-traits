14a21a6c44c1146154d48a64d623a33edb707ce7
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2022-03-02 16:30:16 +0000

    icon update

---
+++ Contents/mods/TradeOffTraits/Media/lua/client/PainTraits.lua
@@ -0,0 +1,14 @@
+ProfessionFramework.addTrait('Masochist', {
+    icon = "trait_Masochist",
+    name = "UI_trait_Masochist",
+    description = "UI_trait_Masochist_desc",
+    exclude = { "Sadist" },
+    cost = 1,
+})
+
+ProfessionFramework.addTrait('Sadist', {
+    icon = "trait_Sadist",
+    name = "UI_trait_Sadist",
+    description = "UI_trait_Sadist_desc",
+    cost = -1,
+})
